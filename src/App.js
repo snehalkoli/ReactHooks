d6aab7647d1616a9e26226af10aff7b11ca084e7
snehalkoli <67510836+snehalkoli@users.noreply.github.com>
2022-08-15 12:11:40 +0530

    counter using hooks initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./styles.css";
-import react from "react";
+import React, { useState } from "react";
 
-const IncNum = () => {
-  console.log("clicked");
-};
 export default function App() {
+  const state = useState();
+  const [count, setCount] = useState(0);
+  const IncNum = () => {
+    setCount(count + 1);
+  };
   return (
     <>
-      <h1>0</h1>
+      <h1>{count}</h1>
       <button onClick={IncNum}>Click Me</button>
     </>
   );
