24b1e95fd6166537a8bf26e88f6fd0a571a0c9b0
snehal koli <snehalkoli1001@gmail.com>
2022-08-15 03:05:57 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./styles.css";
 import react from "react";
-
-const IncNum = () => {
-  console.log("clicked");
-};
+import { useState } from "react/cjs/react.production.min";
 export default function App() {
+ 
+
   return (
     <>
-      <h1>0</h1>
+      <h1>{count}</h1>
       <button onClick={IncNum}>Click Me</button>
     </>
   );
